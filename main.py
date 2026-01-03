f7f13811dabee621884007fdac08ca6bd3cad730
Cherve <ch3rv3@gmail.com>
2026-01-03 02:43:53 -0500

    added groups to main.py, player has two groups now and is drawing and updating through them

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
@@ -22,8 +26,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         delta_time = game_clock.tick(60)
         dt = delta_time / 1000
